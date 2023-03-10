7b58b4312e733b3b1753b64164641eb0ba4bf2d2
PipGrylls <pipgryllsastro@gmail.com>
2023-03-10 16:38:51 +0000

    updating preable, adding lists

--- example_helpers.py
+++ example_helpers.py
@@ -37,3 +37,40 @@ def check_combo(input_op, input_1, input_2):
             exception_cause = f"{ e.__class__ }"
             my_tmp_type = f" { exception_cause[8:-2]}: {e}"
         print(f"type {type(i1)} {input_op} type {type(i2)}, yeilds {my_tmp_type}")
+
+
+def check_fizzbuzz(input_fizzbuzz):
+    correct = True
+    for i in range(0, len(input_fizzbuzz)):
+        if input_fizzbuzz[i] != fizzbuzz(i+1):
+            print(f"You have an error with fizzbuzz element {i}")
+            correct = False
+            break
+    if len(input_fizzbuzz) == 20 and correct:
+        print(f"Success the first 20 numbers fizzbuzzed are \n {input_fizzbuzz}")
+    elif correct:
+        print(f'You dont have all 20 elements you only submitted {len(input_fizzbuzz)}')
+
+
+def fizzbuzz (number_to_fizzbuzz):
+    # check if the modulus of the number is 0 if it is its a multiple!
+    is_number_fizz = number_to_fizzbuzz%3 == 0
+    is_number_buzz = number_to_fizzbuzz%5 == 0
+    # if both multiples then we need to fizzbuzz
+    is_number_fizzbuzz = is_number_fizz and is_number_buzz
+
+    # Check fizzbuzz first 
+    # Then use two elif conditionals to check fizz then buzz
+    # Finally use else to print the number if none of the conditionals pass.
+
+    if(is_number_fizzbuzz):
+        ret = 'fizzbuzz'
+    elif(is_number_fizz):
+        ret = 'fizz'
+    elif(is_number_buzz):
+        ret = 'buzz'
+    else:
+        ret = number_to_fizzbuzz
+
+    return ret
+        
